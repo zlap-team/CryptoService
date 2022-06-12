a012c931f4101a5df4728e89516f680d684f2d2d
Gabriel <drozd.gj@gmail.com>
2022-06-12 11:43:14 +0200

    Added CoinGecko controller with endpoint to get all markets

---
+++ CryptoService/Application/DTOs/CoinGecko/MarketDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.DTOs.CoinGecko;
+
+public class MarketDto
+{
+    public string Id { get; set; }
+    public string Symbol { get; set; }
+    public string Name { get; set; }
+    public string Image { get; set; }
+    public double CurrentPrice { get; set; }
+    public double High24h { get; set; }
+    public double Low24h { get; set; }
+    public double PriceChange24h { get; set; }
+    public double PriceChangePercentage24h { get; set; }
+    public DateTime LastUpdated { get; set; }
+}
---
+++ CryptoService/Domain/Models/CoinGecko/MarketExternalApi.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Models.CoinGecko;
+
+public class MarketExternalApi
+{
+    [JsonPropertyName("id")] 
+    public string Id { get; set; }
+
+    [JsonPropertyName("symbol")] 
+    public string Symbol { get; set; }
+
+    [JsonPropertyName("name")] 
+    public string Name { get; set; }
+
+    [JsonPropertyName("image")] 
+    public string Image { get; set; }
+
+    [JsonPropertyName("current_price")] 
+    public double CurrentPrice { get; set; }
+
+    [JsonPropertyName("high_24h")] 
+    public double High24h { get; set; }
+
+    [JsonPropertyName("low_24h")] 
+    public double Low24h { get; set; }
+
+    [JsonPropertyName("price_change_24h")] 
+    public double PriceChange24h { get; set; }
+
+    [JsonPropertyName("price_change_percentage_24h")]
+    public double PriceChangePercentage24h { get; set; }
+
+    [JsonPropertyName("last_updated")] 
+    public DateTime LastUpdated { get; set; }
+}
